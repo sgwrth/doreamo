f01fb354d5c798d161baab88262788d3c6eff7ad
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-12-05 20:25:56 +0100

    * add: DELETE request

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -69,9 +69,27 @@ namespace HPlusSport.API.Controllers
                 if (!_context.Products.Any(p => p.Id == id))
                 {
                     return NotFound();
+                } else
+                {
+                    throw;
                 }
             }
             return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct([FromRoute] int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return Ok(product);
         }
     }
 }
