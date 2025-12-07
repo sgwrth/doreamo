ac9fcae4f7fab637b048f86cf88498196fe5982d
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-12-07 15:43:58 +0100

    * remove: dotnet project * change: repo now monorepo * add: dotnet API * add: React UI

--- Models/Category.cs
+++
@@ -1,9 +0,0 @@
-namespace HPlusSport.API.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public virtual List<Product> Products { get; set; }
-    }
-}
